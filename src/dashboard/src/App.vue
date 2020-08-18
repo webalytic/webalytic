a7b5bfc1876ce1f1ccc2d5dc00db67099af2830e
Dmiriy Rusov <rusov.d.s@gmail.com>
2020-08-18 19:46:57 +0700

    fix import css

--- src/dashboard/src/App.vue
+++ src/dashboard/src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+
 import DashboardLayout from '@/common/layouts/DashboardLayout'
 
 export default {
@@ -30,13 +31,11 @@ export default {
 }
 </script>
 
-<style lang="css">
-  @import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-  @import '../node_modules/vue2-daterange-picker/dist/vue2-daterange-picker.css';
-</style>
+<style src="@fortawesome/fontawesome-free/css/all.min.css"></style>
+<style src="vue2-daterange-picker/dist/vue2-daterange-picker.css"></style>
 
 <style lang="scss">
   @import "./common/assets/scss/colors.scss";
-  @import "../../../node_modules/bootstrap/scss/bootstrap.scss";
+  @import '../../../node_modules/bootstrap/scss/bootstrap.scss';
   @import "./common/assets/scss/sb-admin-2.scss";
 </style>
