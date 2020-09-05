12c943d3c5f01b4df3488f3c56a701bf2c844ac4
Dmiriy Rusov <rusov.d.s@gmail.com>
2020-09-05 23:52:32 +0700

    fix webalytic.js

--- src/web-sdk/webpack.config.js
+++ src/web-sdk/webpack.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './src/index.ts',
